c96797432cb647975b728b64ac90d39ca6e72f20
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-22 11:36:15 +0800

    chore: Code reorganize

--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -3,10 +3,9 @@
 mod command;
 mod config;
 
-use super::{ParseError, ParseErrorType};
-use command::CommandInstruction;
-use config::ConfigInstruction;
-use std::io::BufRead;
+use super::ParseErrorType;
+pub use command::CommandInstruction;
+pub use config::ConfigInstruction;
 
 /// A single instruction
 #[derive(Debug, PartialEq)]
@@ -55,53 +54,6 @@ impl Instruction {
     }
 }
 
-/// A `.cw` script
-#[derive(Debug)]
-pub struct Script {
-    instructions: Vec<Instruction>,
-}
-
-impl Script {
-    pub fn parse(reader: impl BufRead) -> Result<Self, ParseError> {
-        let mut instructions = Vec::new();
-        let mut expect_continuation = false;
-        for (line_number, line) in reader.lines().enumerate() {
-            let line = line.map_err(|err| ParseErrorType::Io(err).with_line(line_number))?;
-            let instruction =
-                Instruction::parse(&line).map_err(|e| e.with_line(line_number + 1))?;
-            // Check for `ExpectedContinuation` and `UnexpectedContinuation`
-            if let Instruction::Command(command_inst) = &instruction {
-                let is_start = command_inst.is_start();
-                if is_start {
-                    if expect_continuation {
-                        return Err(ParseErrorType::ExpectedContinuation.with_line(line_number + 1));
-                    }
-                } else {
-                    if !expect_continuation {
-                        return Err(
-                            ParseErrorType::UnexpectedContinuation.with_line(line_number + 1)
-                        );
-                    }
-                }
-                expect_continuation = command_inst.expect_continuation();
-            } else if expect_continuation {
-                return Err(ParseErrorType::ExpectedContinuation.with_line(line_number + 1));
-            }
-            instructions.push(instruction);
-        }
-        Ok(Self { instructions })
-    }
-
-    pub fn execute(&self) {
-        execute_instructions(&self.instructions);
-    }
-}
-
-fn execute_instructions(instructions: &[Instruction]) {
-    // TODO: Implement this function
-    println!("{:?}", instructions);
-}
-
 mod util {
     use super::ParseErrorType;
     use std::time::Duration;
@@ -140,7 +92,7 @@ mod util {
 mod tests {
     use super::*;
     use crate::ParseErrorType;
-    use std::{io::BufReader, time::Duration};
+    use std::time::Duration;
 
     #[test]
     fn instruction_with_space() {
@@ -255,121 +207,5 @@ mod tests {
         for (input, expected) in durations.iter() {
             assert_eq!(util::parse_duration(input).unwrap(), *expected);
         }
-    }
-
-    #[test]
-    fn script() {
-        let script = r#"
-            @@width 123
-            @height auto
-            %print
-            !marker
-            #comment
-            $single command
-            $command \
-            >continuation
-        "#;
-        let script = script.trim();
-        let script = script.as_bytes();
-        let script = BufReader::new(script);
-        let script = Script::parse(script).unwrap();
-        let expected = vec![
-            Instruction::PersistentConfig(ConfigInstruction::Width(123)),
-            Instruction::TemporaryConfig(ConfigInstruction::Height(0)),
-            Instruction::Print("print".to_string()),
-            Instruction::Marker("marker".to_string()),
-            Instruction::Empty,
-            Instruction::Command(CommandInstruction::parse("single command", true)),
-            Instruction::Command(CommandInstruction::parse("command \\", true)),
-            Instruction::Command(CommandInstruction::parse("continuation", false)),
-        ];
-        assert_eq!(script.instructions, expected);
-    }
-
-    #[test]
-    fn script_unknown_instruction() {
-        let script = r#"
-            @@width 123
-            @height 456
-            %print
-            !marker
-            #comment
-            $command \
-            >continuation
-            unknown
-        "#;
-        let script = script.trim();
-        let script = script.as_bytes();
-        let script = BufReader::new(script);
-        assert!(matches!(
-            Script::parse(script).unwrap_err(),
-            ParseError {
-                error: ParseErrorType::UnknownInstruction,
-                line: 8
-            }
-        ));
-    }
-
-    #[test]
-    fn script_malformed_instruction() {
-        let malformed_scripts = [
-            // Config instruction
-            "#abc\n@",                   // Expected character after @
-            "#abc\n@@",                  // Expected character after @@
-            "#abc\n@@wid",               // Unrecognized configuration instruction
-            "@@width 123\n@@height abc", // Malformed integer
-            "@@width 123\n@idle 1",      // Malformed duration - no suffix
-            "@@width 123\n@idle 1min",   // Malformed duration - invalid suffix
-            "@@width 123\n@delay",       // Malformed duration - no value
-            "@@width 123\n@hidden what", // Malformed boolean
-        ];
-        for script in malformed_scripts.iter() {
-            let script = script.trim();
-            let script = script.as_bytes();
-            let script = BufReader::new(script);
-            assert!(matches!(
-                Script::parse(script).unwrap_err(),
-                ParseError {
-                    error: ParseErrorType::MalformedInstruction,
-                    line: 2
-                }
-            ));
-        }
-    }
-
-    #[test]
-    fn script_expected_continuation() {
-        let script = r#"
-            $command \
-            @width 123
-        "#;
-        let script = script.trim();
-        let script = script.as_bytes();
-        let script = BufReader::new(script);
-        assert!(matches!(
-            Script::parse(script).unwrap_err(),
-            ParseError {
-                error: ParseErrorType::ExpectedContinuation,
-                line: 2
-            }
-        ));
-    }
-
-    #[test]
-    fn script_unexpected_continuation() {
-        let script = r#"
-            $command
-            >continuation
-        "#;
-        let script = script.trim();
-        let script = script.as_bytes();
-        let script = BufReader::new(script);
-        assert!(matches!(
-            Script::parse(script).unwrap_err(),
-            ParseError {
-                error: ParseErrorType::UnexpectedContinuation,
-                line: 2
-            }
-        ));
     }
 }
