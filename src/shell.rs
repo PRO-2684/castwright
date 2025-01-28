26fa6b4c4c71a90b27e12b02eac176da545b1085
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-28 10:22:28 +0800

    feat: Using duct

--- src/shell.rs
+++ src/shell.rs
@@ -1,61 +1,42 @@
 //! Module for interacting with the shell.
 
-use std::process::{Command, Stdio};
-use std::io::{self, Read, Write};
-use std::time::Duration;
-use std::thread;
-use std::sync::mpsc::{channel, Sender, Receiver};
+use duct::{cmd, ReaderHandle};
+use std::{io::Read, time::Duration};
 
 use super::ErrorType;
 
 const INTERVAL: Duration = Duration::from_millis(10);
 
-/// Read from a source and send the output to a channel.
-fn poll(mut source: impl Read, dest: Sender<String>) {
+/// Execute a command using given shell, returning its output as a receiver.
+fn execute_command(command: &str, shell: &str) -> Result<ReaderHandle, ErrorType> {
+    // Spawn the command
+    let command = cmd!(shell, "-c", command);
+    let reader = command.stderr_to_stdout().reader().map_err(ErrorType::Io)?;
+
+    Ok(reader)
+}
+
+/// Poll the reader for output and error.
+fn poll_reader(
+    reader: &mut impl Read,
+    mut on_output: impl FnMut(String),
+    on_error: impl FnOnce(std::io::Error),
+) {
     let mut buffer = [0; 1024];
     loop {
-        match source.read(&mut buffer) {
+        match reader.read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
                 let output = String::from_utf8_lossy(&buffer[..n]).to_string();
-                if let Err(err) = dest.send(output) {
-                    eprintln!("Failed to send output: {}", err);
-                    break;
-                }
+                on_output(output);
             }
             Err(e) => {
-                eprintln!("Error reading: {}", e);
+                on_error(e);
                 break;
             }
         }
-        thread::sleep(INTERVAL);
+        std::thread::sleep(INTERVAL);
     }
-}
-
-/// Execute a command using given shell, returning its output as a receiver.
-fn execute_command(command: &str, shell: &str) -> Result<Receiver<String>, ErrorType> {
-    // Spawn the command
-    let mut child = Command::new(shell)
-        .arg("-c")
-        .arg(command)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .map_err(ErrorType::Io)?;
-
-    // Get the stdout and stderr
-    let stdout = child.stdout.take().expect("Failed to get stdout");
-    let stderr = child.stderr.take().expect("Failed to get stderr");
-
-    // Create a channel to send the output
-    let (tx_stdout, rx) = channel();
-    let tx_stderr = tx_stdout.clone();
-
-    // Send the output to the channel
-    thread::spawn(move || poll(stdout, tx_stdout));
-    thread::spawn(move || poll(stderr, tx_stderr));
-
-    Ok(rx)
 }
 
 #[cfg(test)]
@@ -66,8 +47,9 @@ mod tests {
     fn echo_stdout() {
         let command = "echo hello";
         let shell = "bash";
-        let output = execute_command(command, shell).unwrap();
-        let output = output.recv().unwrap();
+        let mut reader = execute_command(command, shell).unwrap();
+        let mut output = String::new();
+        reader.read_to_string(&mut output).unwrap();
         assert_eq!(output, "hello\n");
     }
 
@@ -75,8 +57,9 @@ mod tests {
     fn echo_stderr() {
         let command = "echo hello 1>&2";
         let shell = "bash";
-        let output = execute_command(command, shell).unwrap();
-        let output = output.recv().unwrap();
+        let mut reader = execute_command(command, shell).unwrap();
+        let mut output = String::new();
+        reader.read_to_string(&mut output).unwrap();
         assert_eq!(output, "hello\n");
     }
 
@@ -84,12 +67,11 @@ mod tests {
     fn echo_both() {
         let command = "echo hello; echo world 1>&2";
         let shell = "bash";
-        let output = execute_command(command, shell).unwrap();
-        let expected = vec!["hello\n", "world\n"];
-        let mut actual = Vec::new();
-        for message in output.iter() {
-            actual.push(message);
-        }
+        let mut reader = execute_command(command, shell).unwrap();
+        let expected = "hello\nworld\n";
+        let mut actual = String::new();
+        reader.read_to_string(&mut actual).unwrap();
+
         assert_eq!(actual, expected);
     }
 
@@ -97,25 +79,38 @@ mod tests {
     fn echo_with_delay() {
         let command = "echo hello; sleep 1; echo world 1>&2";
         let shell = "bash";
-        let output = execute_command(command, shell).unwrap();
+        let mut reader = execute_command(command, shell).unwrap();
         let expected = vec!["hello\n", "world\n"];
         let mut actual = Vec::new();
 
         let mut first = None;
         let mut second = None;
 
-        for message in output.iter() {
-            actual.push(message);
-            if first.is_none() {
-                first = Some(std::time::Instant::now());
-            } else if second.is_none() {
-                second = Some(std::time::Instant::now());
-            }
-        }
+        poll_reader(
+            &mut reader,
+            |output| {
+                actual.push(output);
+                if first.is_none() {
+                    first = Some(std::time::Instant::now());
+                } else {
+                    second = Some(std::time::Instant::now());
+                }
+            },
+            |e| eprintln!("Error reading: {}", e),
+        );
+
         assert_eq!(actual, expected);
 
         let duration = second.unwrap().duration_since(first.unwrap());
-        assert!(duration >= Duration::from_secs(1), "Duration: {:?}", duration);
-        assert!(duration <= Duration::from_secs(1) + INTERVAL, "Duration: {:?}", duration);
+        assert!(
+            duration >= Duration::from_secs(1),
+            "Duration: {:?}",
+            duration
+        );
+        assert!(
+            duration <= Duration::from_secs(1) + 2 * INTERVAL,
+            "Duration: {:?}",
+            duration
+        );
     }
 }
